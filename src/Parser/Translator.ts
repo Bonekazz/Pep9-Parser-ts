7f6bf2e98dafbb08b4b22f97afd019d7e2b046da
Bonekazz <juracirdantas@gmail.com>
2023-11-17 09:07:18 -0300

    finished all states and translate func

--- src/Parser/Translator.ts
+++ src/Parser/Translator.ts
@@ -1,16 +1,15 @@
 import ACode, { EmptyInstruction, NonUnaryInstruction, UnaryInstruction, hiError } from "./ACode";
 import InBuffer from "../Lex-Analyzer/src/InBuffer";
 import Tokenizer from "../Lex-Analyzer/src/Tokenizer";
-import AArg, { IntArg } from "./AArg";
+import AArg, { AddrModeArg, HexArg, IdentifierArg, IntArg } from "./AArg";
 import { AddressingMode, NonUnary1, NonUnary2, Unary } from "./HashMaps";
-import AToken, { TEmpty, TIdentifier } from "../Lex-Analyzer/src/Tokens";
+import AToken, { TAddress, TEmpty, THex, TIdentifier, TInteger } from "../Lex-Analyzer/src/Tokens";
 import Maps from "./HashMaps"
 
 export enum ParseState{
-    START, UNARY, NONUNARY1, NONUNARY2, PARAM1, PARAM2, FINISH
+    START, UNARY, NONUNARY, PARAM1, PARAM2, FINISH
 }
 
-type MnemonTypes = Unary | NonUnary1 | NonUnary2 | AddressingMode | undefined;
 
 export default class Translator {
     private buffer: InBuffer;
@@ -24,10 +23,11 @@ export default class Translator {
 
     private parseLine(): boolean {
         let terminate = false;
-        let localFirstArg = new IntArg(0);
-        let localSecondArg: AArg;
+        let localFirstArg: any = new HexArg(0);
+        let localSecondArg: any = new IdentifierArg("");
+        let tempString: string;
         // Compiler requires following useless initialization.
-        let localMnemon: MnemonTypes = Unary.dotEND;
+        let localMnemon: any = Unary.dotEND;
         let aToken: AToken;
         this.aCode = new EmptyInstruction();
         let state = ParseState.START;
@@ -37,7 +37,7 @@ export default class Translator {
             switch (state) {
                 case ParseState.START:
                     if (aToken instanceof TIdentifier) {
-                        let tempString = aToken.getStringValue().toLowerCase();
+                        tempString = aToken.getStringValue().toLowerCase();
 
                         if (Maps.unaryMnemonTable.has(tempString)) {
                             localMnemon = Maps.unaryMnemonTable.get(tempString);
@@ -49,13 +49,13 @@ export default class Translator {
 
                         if (Maps.nonUnary1MnemonTable.has(tempString)) {
                             localMnemon = Maps.nonUnary1MnemonTable.get(tempString);
-                            state = ParseState.NONUNARY1;
+                            state = ParseState.NONUNARY;
                             break;
                         }
 
                         if (Maps.nonUnary2MnemonTable.has(tempString)) {
                             localMnemon = Maps.nonUnary2MnemonTable.get(tempString);
-                            state = ParseState.NONUNARY2;
+                            state = ParseState.NONUNARY;
                             break;
                         }
 
@@ -82,9 +82,42 @@ export default class Translator {
                     this.aCode = new hiError("Unary instructions cannot have arguments.")
                     break;
                 
-                case ParseState.NONUNARY1:
+                case ParseState.NONUNARY:
+                    if (aToken instanceof THex) {
+                        localFirstArg = new HexArg(aToken.getIntValue());
+                        state = ParseState.PARAM1;
+                        break;
+                    }
+                    
+                    this.aCode = new hiError("invalid parameter");
                     break;
                 
+                case ParseState.PARAM1:
+                    if (aToken instanceof TEmpty && localMnemon in NonUnary1) {
+                        this.aCode = new NonUnaryInstruction(localMnemon, localFirstArg);
+                        state = ParseState.FINISH;
+                        break;
+                    }
+
+                    if (aToken instanceof TAddress) {
+                        localSecondArg = new AddrModeArg(aToken.getStringValue())
+                        state = ParseState.PARAM2;
+                        break;
+                    }
+
+                    this.aCode = new hiError("missing an valid argument")
+                    break;
+                
+                case ParseState.PARAM2:
+                    if (aToken instanceof TEmpty) {
+                        this.aCode = new NonUnaryInstruction(localMnemon, localFirstArg, localSecondArg);
+                        state = ParseState.FINISH;
+                        break;
+                    }
+
+                    this.aCode = new hiError("this instruction doesn't accept an extra argument")
+                    break;
+
                 default:
                     state = ParseState.FINISH;
                     this.aCode = new hiError("something went wrong");
@@ -96,6 +129,48 @@ export default class Translator {
         } while (state !== ParseState.FINISH && !(this.aCode instanceof hiError));
 
         return terminate;
+    }
+
+    translate(): void {
+        const codeTable: ACode[] = [];
+        let numErrors = 0;
+        let hasEnd = false;
+        this.buffer.cutFirstLine();
+
+        while (this.buffer.inputRemains() && !hasEnd) {
+            hasEnd = this.parseLine(); // sets ACode and returns boolean;
+            codeTable.push(this.aCode);
+            if (this.aCode instanceof hiError) {
+                numErrors++
+            }
+            this.buffer.cutFirstLine();
+        }
+
+        if (!hasEnd) {
+            this.aCode = new hiError("Missing .END sentinel");
+            codeTable.push(this.aCode);
+            numErrors++;
+        }
+
+        if (numErrors == 0) {
+            console.log("Object code:\n");
+            for (let i = 0; i < codeTable.length; i++) {
+                console.log(`${codeTable[ i ].generateCode()}`);
+            }
+        }
+
+        if (numErrors == 1) {
+            console.log("One error was detected.\n");
+        }
+
+        if (numErrors > 1) {
+            console.log(`${numErrors} errors were detected.\n`);
+        }
+
+        console.log("\nProgram listing:\n");
+        for (let i = 0; i < codeTable.length; i++) {
+            console.log(`${codeTable[ i ].generateListing()}`);
+        }
     }
 
 }
