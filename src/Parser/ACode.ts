109d5038cc88977e365e163a7319c7a3a0670127
Bonekazz <juracirdantas@gmail.com>
2023-11-18 08:12:41 -0300

    fix generateListing Output

--- src/Parser/ACode.ts
+++ src/Parser/ACode.ts
@@ -1,4 +1,4 @@
-import AArg, { HexArg, IdentifierArg } from "./AArg";
+import AArg, { HexArg, IdentifierArg, IntArg } from "./AArg";
 import Maps, { Unary, NonUnary1, NonUnary2} from "./HashMaps";
 
 export default abstract class ACode {
@@ -40,14 +40,16 @@ export class EmptyInstruction extends ACode {
 
 export class UnaryInstruction extends ACode {
     private mnemonic: Unary | undefined;
+    private stringMnemonic: any;
 
     constructor(mn: Unary | undefined) {
         super();
         this.mnemonic = mn;
+        this.stringMnemonic = Maps.unaryStringTable.get(this.mnemonic);
     }
 
     override generateListing(): string {
-        return `${Maps.unaryStringTable.get(this.mnemonic)}\n`;
+        return `${Maps.unaryStringTable.get(this.mnemonic)}`;
     }
 
     override generateCode(): string {
@@ -58,22 +60,33 @@ export class UnaryInstruction extends ACode {
 
 export class NonUnaryInstruction extends ACode {
     private mnemonic: any;
+    private stringMnemonic: any;
     private opEspeci: AArg;
-    private addrMode: AArg | undefined;
+    private addrMode: any;
 
     constructor(mn: NonUnary1 | NonUnary2, operand: AArg, addrmode?: AArg) {
         super();
         this.mnemonic = mn;
+        this.stringMnemonic = Maps.nonUnary1StringTable.get(this.mnemonic) || Maps.nonUnary2StringTable.get(this.mnemonic);        
         this.opEspeci = operand;
         this.addrMode = addrmode;
+        
     }
 
     override generateListing(): string {
         if (this.mnemonic in NonUnary1) {
-            return `${Maps.nonUnary1StringTable.get(this.mnemonic)}    ${this.opEspeci.generateCode()}`
+            if (this.opEspeci instanceof IntArg) {
+                return `${Maps.nonUnary1StringTable.get(this.mnemonic)}    ${this.opEspeci.generateCode()}`
+            }
+            return `${Maps.nonUnary1StringTable.get(this.mnemonic)}    0x${this.opEspeci.generateCode()}`
         }
 
-        return`${Maps.nonUnary2StringTable.get(this.mnemonic)}    0x${this.opEspeci.generateCode()}, ${Maps.addrModeStringTable.get(this.addrMode)}`
+        if (this.opEspeci instanceof IntArg) {
+            return `${Maps.nonUnary2StringTable.get(this.mnemonic)}    ${this.opEspeci.generateCode()}, ${this.addrMode.generateCode()}`
+        }
+
+        return `${Maps.nonUnary2StringTable.get(this.mnemonic)}    0x${this.opEspeci.generateCode()}, ${this.addrMode.generateCode()}`
+        
 
     }
 
